8bf27ed5422ebfd6afda1d20d8ea636271d32197
rokeycb4 <rokeybcb4@gmail.com>
2025-05-19 13:45:49 +0900

    Update task1.py

--- 0519/task1.py
+++ 0519/task1.py
@@ -32,12 +32,14 @@ def movelz(z):
     
 def grab_place(src, des):
 
+    
     upheight = 100
 
     height = get_current_posx()[0][2] - src[2]
 
-    src[2] = get_current_posx()[0][2]
-    movel(src)
+    src_ = trans(src,[0,0,0, 0,0,0], DR_BASE, DR_BASE)
+    src_[2] = get_current_posx()[0][2]
+    movel(src_)
     
     # 내려서 잡고 올리기
     movel([0,0,-height, 0,0,0], mod=1)
@@ -49,8 +51,9 @@ def grab_place(src, des):
     height = get_current_posx()[0][2] - des[2]
     
      # z값 유지하면서 이동
-    des[2] = get_current_posx()[0][2]
-    movel(des)
+    des_ = trans(des,[0,0,0, 0,0,0], DR_BASE, DR_BASE)
+    des_[2] = get_current_posx()[0][2]
+    movel(des_)
     wait(0.5)
     
     # 내리고 놓고 올리기
@@ -58,24 +61,34 @@ def grab_place(src, des):
 
 def pulloff():
     """레고 뽑아서 들기"""
+    grab()
+    wait(1)
     force_control_on(10)
     
-    
-    movel([0,0,0,  0,8,0], 3,3, mod=1)
-    
-    for i in range(3):
-    break
-        curr = get_current_posx()[0]
-        curr[4] += 3
-        movel(curr, 10, 10)
-        wait(0.1)
+    movel([0,0,0,  0,8,0], 1,1, mod=1)
+    wait(1)
     
     force_control_off()
+    movel([0,0,0,  0,-8,0], 1,1, mod=1)
 
-def pullin(pos1, pos2):
+    
+
+def pullin(pos1, pos2, h):
     """레고 끼우기"""
+    grab_place(pos1,pos2)
     
-    
+    force_control_on(-20)
+    while True:
+        t = get_tool_force()
+        if t[2] > 20:
+            z = get_current_posx()[0][2]
+            if z < h:
+                force_control_off()
+                break
+    release()            
+    #movelz(30)
+    wait(1)
+               
 
     
     
@@ -94,22 +107,12 @@ release()
 
 p1 = posx(422.71, -228.1, 11.43, 152.36, 180, 151.34)   #시작 위치
 p2 = posx(518.75, 21.5, 16.87, 0.65, -179.96, -0.51)     #레고1 (힘제어 시작위치니까 살짝 위에 있어야됨)
-p3 = posx(518.75, 21.5, , 50, -179.96, -0.51)            #레고2
+p3 = posx(518.75, 21.5, 50, 0.65, -179.96, -0.51)            #레고2
 
 
-grab_place(p1,p2)
 
-force_control_on(-20)
-while True:
-    t = get_tool_force()
-    if t[2] > 20:
-        z = get_current_posx()[0][2]
-        if z < 15:
-            force_control_off()
-            break
-            
-wait(1)
-
+pullin(p1, p2, 15)
+pullin(p1, p3, 33)
 pulloff()
-    
 
+    
