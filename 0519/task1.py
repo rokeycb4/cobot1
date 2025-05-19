f745f5e8d2ced29ca08a7e7934fb17285cd24329
Inwoo Kang <mingureion@gmail.com>
2025-05-19 10:18:55 +0900

    Update task1.py

--- 0519/task1.py
+++ 0519/task1.py
@@ -1,1 +1,13 @@
 ## 레고조립1
+
+
+def force_control_on():
+    k_d = [500.0,500.0,500.0,  200.0,200.0,200.0]
+    task_compliance_ctrl(k_d)
+    f_d = [0,0,-20,0.0,0.0,0.0]
+    f_dir = [0,0,1,0,0,0]
+    set_desired_force(f_d,f_dir)
+
+def force_control_off():
+    release_force()
+    release_compliance_ctrl()
