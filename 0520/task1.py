2c03441b2e5a69dd11c872cf6c23072db22a5853
rokeycb4 <rokeybcb4@gmail.com>
2025-05-20 11:18:45 +0900

    Update task1.py

--- 0520/task1.py
+++ 0520/task1.py
@@ -1,13 +1,19 @@
-##블럭 정렬
+def set_va(jvel, jacc,xvel, xacc):
+    """속도 가속도 초기화"""
+    set_velj(jvel)
+    set_accj(jacc)
+    set_velx(xvel)
+    set_accx(xacc)
+
 def grab():
-    set_digital_output(1,1)
+    set_digital_output(1,0)
     set_digital_output(2,1)
     wait(1)
 
 def release():
     set_digital_output(2,0)
     set_digital_output(1,1)
-    
+
     
 def force_control_on():
     k_d = [3000.0,3000.0,3000.0,  200.0,200.0,200.0]
@@ -21,6 +27,9 @@ def force_control_off():
     release_force()
     release_compliance_ctrl()
     
+def movelz(z):
+    """z축 상대좌표 이동"""
+    movel([0,0,z,  0,0,0], 60, 60, mod=1)         
     
 # src에서 잡아서 des에 놓기
 def grab_place(src, des, upheight, upheightd, obj='cup'):
@@ -36,7 +45,7 @@ def grab_place(src, des, upheight, upheightd, obj='cup'):
     
     # 내려서 잡고 올리기
     movel([0,0,-height, 0,0,0], mod=1)
-    grab(obj)
+    grab()
     wait(0.5)
     movel([0,0,upheight, 0,0,0], mod=1)
     
@@ -50,6 +59,12 @@ def grab_place(src, des, upheight, upheightd, obj='cup'):
     
     # 내리고 놓고 올리기
     movelz(-height)
+    force_control_on()
+    while True:
+        t = get_tool_force()
+        if t[2] > 20:
+            force_control_off()
+            break
     release()
     movelz(upheightd)
     
@@ -67,34 +82,37 @@ def get_height(pos):
         if t[2] > 20:
             current_height = get_current_posx()[0][2]
             force_control_off()
+            
+            movelz(30)
+            release()
+
             return current_height
             
+        
 
         
 def get_next_des(h):
  """높이에 맞는 다음 도착지 구하기"""
+ global high_idx, mid_idx, low_idx
+ 
+ if h > middle+5:
+    idx = high_idx
+    high_idx += 1
+ elif h > short+5:
+    idx = mid_idx
+    mid_idx += 1
+ else:
+    idx = low_idx
+    low_idx += 1
 
-
-    global high_idx, mid_idx, low_idx
-
-    if h > middle+5:
-        idx = high_idx
-        high_idx += 1
-    elif h > short+5:
-        idx = mid_idx
-        mid_idx += 1
-    else:
-        idx = low_idx
-        low_idx += 1
-
-    return des_cord[idx]
+ return des_cord[idx]
             
     
     
 ####################################################  좌표 구하기
 
 
-a11 = posx(100, 200, 300, 0, 180, 0)  # 예시값 (x=100, y=200, z=300 등)
+a11 = posx(400.14, 48.31, 85.79, 8.41, -180, 7.56) # 예시값 (x=100, y=200, z=300 등)
 
 # 블록 간 간격
 interval = 51
@@ -111,7 +129,7 @@ for row in range(3):  # 행
 
     
 # 초기 좌표 b11
-b11 = posx(400, 500, 300, 0, 180, 0)  # 예시값 (x=400, y=500, z=300 등)
+b11 = posx(395.59, -150.6, 81.76, 156.83, 180, 154.97)  # 예시값 (x=400, y=500, z=300 등)
 
 # des_cord 생성
 des_cord = []
@@ -129,18 +147,17 @@ mid_idx = 3
 low_idx = 6
 
 # 각 블럭 높이
-short = 0
-middle = 0
+short = 50.99
+middle = 60.61
 #tall = 0
 
 ##################################################### code start
-
+set_va(100, 100, 100, 100)
 
 home_j = posj(0,0,90, 0,90,0)             # 초기위치
 home = posx(367.3, 7.07, 204.32, 89.42, 179.98, 89.33)
-movel(home)
-
-
+movej(home_j)
+release()
 # 정렬 시작
 for src in src_cord:
     movel(home)  # 홈 포지션으로 복귀 (안정성 확보)
@@ -151,5 +168,10 @@ for src in src_cord:
     des = get_next_des(h)
 
     # 옮기기
-    grab_place(src, des, upheight=50, upheightd=50)
+    curr = get_current_posx()[0]
+    curr[2] -= 50
+    grab_place(curr, des, upheight=80, upheightd=50)
+    
+    #
+
 
